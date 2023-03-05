29bb70f10a7d2bcdf7fa91d4c5c59091a1db2c31
Daniel Haim <2808046+danielhaim1@users.noreply.github.com>
2023-03-05 19:31:24 +0100

    v1.0.5

--- index.js
+++ index.js
@@ -1,76 +1,80 @@
 const uppercaseRegex = /[A-Z]/;
 const abbreviationRegex = /\../;
 
-String.prototype.toTitleCase = function (options) {
-  try {
-    if (!(this instanceof String)) {
-      throw new TypeError('Invalid input: input must be a string.');
-    }
-
-    if (typeof options !== 'undefined' && typeof options !== 'object') {
-      throw new TypeError('Invalid options: options must be an object.');
-    }
-
-    const defaultOptions = {
-      neverCapitalized: ["etc.", "i.e.", "e.g.", "vs.", "etc"],
-      shortConjunctions: ["and", "as", "but", "for", "if", "nor", "or", "so", "yet"],
-      articles: ["a", "an", "the"],
-      shortPrepositions: ["as", "at", "by", "for", "in", "of", "off", "on", "per", "to", "up", "via"],
-    };
-
-    const chicagoOptions = {
-      neverCapitalized: ["a", "an", "the", "and", "but", "or", "for", "nor", "on", "at", "to", "from", "by", "with", "in", "of"],
-      shortConjunctions: [],
-      articles: ["a", "an", "the"],
-      shortPrepositions: ["as", "at", "by", "for", "in", "of", "on", "to", "up"],
-    }
-
-    const mergedOptions = options?.style === "chicago" ?
-      { ...defaultOptions, ...chicagoOptions } :
-      { ...defaultOptions, ...options };
-
-    const capitalizeWord = (word) => {
-      if (mergedOptions.shortConjunctions.includes(word) || mergedOptions.articles.includes(word) || mergedOptions.shortPrepositions.includes(word)) {
-        return word.toLowerCase();
-      } else if (word.includes(":")) {
-        const nextWordIndex = words.indexOf(word) + 1;
-        if (nextWordIndex < words.length) {
-          words[nextWordIndex] = words[nextWordIndex].charAt(0).toUpperCase() + words[nextWordIndex].slice(1);
+String.prototype.toTitleCase = function(options) {
+    try {
+        if (!(this instanceof String)) {
+            throw new TypeError('Invalid input: input must be a string.');
         }
-        return word.charAt(0).toUpperCase() + word.slice(1);
-      } else if (word.includes("-")) {
-        const hyphenatedWord = word.split("-");
-        const capitalizedHyphenatedWord = hyphenatedWord.map((word, index) => {
-          if (index === hyphenatedWord.length - 1 && word.match(/^(IV|VI{0,3}|IX|XI{0,3}|XIV|XV|XVI{0,2}|XVII|XIX|[IVX]+)$/i)) {
-            return word.toUpperCase();
-          } else {
-            return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
-          }
-        }).join("-");
-        return capitalizedHyphenatedWord;
-      } else {
-        if (uppercaseRegex.test(word.slice(1)) || abbreviationRegex.test(word.slice(1))) {
-          return word;
-        } else if (mergedOptions.neverCapitalized.includes(word)) {
-          return word.toLowerCase();
-        } else {
-          return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+
+        if (typeof options !== 'undefined' && typeof options !== 'object') {
+            throw new TypeError('Invalid options: options must be an object.');
         }
-      }
+
+        const defaultOptions = {
+            neverCapitalized: ["etc.", "i.e.", "e.g.", "vs.", "etc"],
+            shortConjunctions: ["and", "as", "but", "for", "if", "nor", "or", "so", "yet"],
+            articles: ["a", "an", "the"],
+            shortPrepositions: ["as", "at", "by", "for", "in", "of", "off", "on", "per", "to", "up", "via"],
+        };
+
+        const chicagoOptions = {
+            neverCapitalized: ["a", "an", "the", "and", "but", "or", "for", "nor", "on", "at", "to", "from", "by", "with", "in", "of"],
+            shortConjunctions: [],
+            articles: ["a", "an", "the"],
+            shortPrepositions: ["as", "at", "by", "for", "in", "of", "on", "to", "up"],
+        }
+
+        const mergedOptions = options?.style === "chicago" ? {
+            ...defaultOptions,
+            ...chicagoOptions
+        } : {
+            ...defaultOptions,
+            ...options
+        };
+
+        const capitalizeWord = (word) => {
+            if (mergedOptions.shortConjunctions.includes(word) || mergedOptions.articles.includes(word) || mergedOptions.shortPrepositions.includes(word)) {
+                return word.toLowerCase();
+            } else if (word.includes(":")) {
+                const nextWordIndex = words.indexOf(word) + 1;
+                if (nextWordIndex < words.length) {
+                    words[nextWordIndex] = words[nextWordIndex].charAt(0).toUpperCase() + words[nextWordIndex].slice(1);
+                }
+                return word.charAt(0).toUpperCase() + word.slice(1);
+            } else if (word.includes("-")) {
+                const hyphenatedWord = word.split("-");
+                const capitalizedHyphenatedWord = hyphenatedWord.map((word, index) => {
+                    if (index === hyphenatedWord.length - 1 && word.match(/^(IV|VI{0,3}|IX|XI{0,3}|XIV|XV|XVI{0,2}|XVII|XIX|[IVX]+)$/i)) {
+                        return word.toUpperCase();
+                    } else {
+                        return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+                    }
+                }).join("-");
+                return capitalizedHyphenatedWord;
+            } else {
+                if (uppercaseRegex.test(word.slice(1)) || abbreviationRegex.test(word.slice(1))) {
+                    return word;
+                } else if (mergedOptions.neverCapitalized.includes(word)) {
+                    return word.toLowerCase();
+                } else {
+                    return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+                }
+            }
+        }
+
+        const words = this.split(" ");
+        const capitalizedWords = words.map((word, index) => {
+            if (index === 0 || index === words.length - 1) {
+                return capitalizeWord(word.charAt(0).toUpperCase() + word.slice(1));
+            } else {
+                return capitalizeWord(word);
+            }
+        });
+
+        return capitalizedWords.join(" ");
+
+    } catch (error) {
+        throw new TypeError(`Invalid argument: ${error.message}`);
     }
-
-    const words = this.split(" ");
-    const capitalizedWords = words.map((word, index) => {
-      if (index === 0 || index === words.length - 1) {
-        return capitalizeWord(word.charAt(0).toUpperCase() + word.slice(1));
-      } else {
-        return capitalizeWord(word);
-      }
-    });
-
-    return capitalizedWords.join(" ");
-
-  } catch (error) {
-    throw new TypeError(`Invalid argument: ${error.message}`);
-  }
 };
--- test/index.test.js
+++ test/index.test.js
@@ -1,64 +1,74 @@
-const { describe, test, expect } = require('@jest/globals');
+const {
+    describe,
+    test,
+    expect
+} = require('@jest/globals');
 
-const { toTitleCase } = require('../index');
+const {
+    toTitleCase
+} = require('../index');
 
 describe('String.prototype.toTitleCase', () => {
 
-  test('throws TypeError if input is not a string', () => {
-    expect(() => {
-      toTitleCase(null);
-    }).toThrow(TypeError);
-  });
+    test('throws TypeError if input is not a string', () => {
+        expect(() => {
+            toTitleCase(null);
+        }).toThrow(TypeError);
+    });
 
-  test('throws TypeError if options is not an object', () => {
-    expect(() => {
-      'hello world'.toTitleCase('invalid option');
-    }).toThrow(TypeError);
-  });
+    test('throws TypeError if options is not an object', () => {
+        expect(() => {
+            'hello world'.toTitleCase('invalid option');
+        }).toThrow(TypeError);
+    });
 
-  test('capitalizes the first letter of each word in a sentence', () => {
-    const result = 'train your mind for peak performance: a science-based approach for achieving your goals'.toTitleCase();
-    expect(result).toBe('Train Your Mind for Peak Performance: A Science-Based Approach for Achieving Your Goals');
-  });
+    test('capitalizes the first letter of each word in a sentence', () => {
+        const result = 'train your mind for peak performance: a science-based approach for achieving your goals'.toTitleCase();
+        expect(result).toBe('Train Your Mind for Peak Performance: A Science-Based Approach for Achieving Your Goals');
+    });
 
-  test('capitalizes the first letter of the first word in a sentence', () => {
-    const result = 'TRAIN your mind for peak performance'.toTitleCase();
-    expect(result).toBe('TRAIN Your Mind for Peak Performance');
-  });
+    test('capitalizes the first letter of the first word in a sentence', () => {
+        const result = 'TRAIN your mind for peak performance'.toTitleCase();
+        expect(result).toBe('TRAIN Your Mind for Peak Performance');
+    });
 
-  test('handles hyphenated words and last words in a sentence', () => {
-    const result = 'turning frowns (and smiles) upside down: a multilevel examination of surface acting positive and negative emotions on well-being'.toTitleCase();
-    expect(result).toBe('Turning Frowns (and Smiles) Upside Down: A Multilevel Examination of Surface Acting Positive and Negative Emotions on Well-Being');
-  });
+    test('handles hyphenated words and last words in a sentence', () => {
+        const result = 'turning frowns (and smiles) upside down: a multilevel examination of surface acting positive and negative emotions on well-being'.toTitleCase();
+        expect(result).toBe('Turning Frowns (and Smiles) Upside Down: A Multilevel Examination of Surface Acting Positive and Negative Emotions on Well-Being');
+    });
 
-  test('capitalizes the first word of a sentence', () => {
-    const result = 'the quick brown fox jumps over the lazy dog'.toTitleCase();
-    expect(result).toBe('The Quick Brown Fox Jumps Over the Lazy Dog');
-  });
+    test('capitalizes the first word of a sentence', () => {
+        const result = 'the quick brown fox jumps over the lazy dog'.toTitleCase();
+        expect(result).toBe('The Quick Brown Fox Jumps Over the Lazy Dog');
+    });
 
-  test('capitalizes all significant words in a sentence', () => {
-    const result = 'to be or not to be'.toTitleCase();
-    expect(result).toBe('To Be or Not to Be');
-  });
+    test('capitalizes all significant words in a sentence', () => {
+        const result = 'to be or not to be'.toTitleCase();
+        expect(result).toBe('To Be or Not to Be');
+    });
 
-  test('excludes specific words from capitalization', () => {
-    const result = 'the name of the musical is The Musical'.toTitleCase({ neverCapitalized: ["The Musical"] });
-    expect(result).toBe('The Name of the Musical Is The Musical');
-  });
+    test('excludes specific words from capitalization', () => {
+        const result = 'the name of the musical is The Musical'.toTitleCase({
+            neverCapitalized: ["The Musical"]
+        });
+        expect(result).toBe('The Name of the Musical Is The Musical');
+    });
 
-  test('capitalizes significant words with Chicago style', () => {
-    const myString = "the quick rabbit together with the brown fox jumped over the dog";
-    const result = myString.toTitleCase({ style: "chicago" });
-    expect(result).toBe('The Quick Rabbit Together with the Brown Fox Jumped Over the Dog');
-  });
+    test('capitalizes significant words with Chicago style', () => {
+        const myString = "the quick rabbit together with the brown fox jumped over the dog";
+        const result = myString.toTitleCase({
+            style: "chicago"
+        });
+        expect(result).toBe('The Quick Rabbit Together with the Brown Fox Jumped Over the Dog');
+    });
 
-  test('does not modify already capitalized words', () => {
-    const result = 'The Quick Brown Fox Jumps Over the Lazy Dog'.toTitleCase();
-    expect(result).toBe('The Quick Brown Fox Jumps Over the Lazy Dog');
-  });
+    test('does not modify already capitalized words', () => {
+        const result = 'The Quick Brown Fox Jumps Over the Lazy Dog'.toTitleCase();
+        expect(result).toBe('The Quick Brown Fox Jumps Over the Lazy Dog');
+    });
 
-  test('returns an empty string for empty input', () => {
-    const result = ''.toTitleCase();
-    expect(result).toBe('');
-  });
+    test('returns an empty string for empty input', () => {
+        const result = ''.toTitleCase();
+        expect(result).toBe('');
+    });
 });
