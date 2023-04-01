f51ec4ce1b93287a2a0befa1b178d01329465fd2
Daniel Haim <2808046+danielhaim1@users.noreply.github.com>
2023-04-01 23:34:50 +0200

    Comments

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
-// Import the `TitleCaser` class
 import { TitleCaser } from './TitleCaser.js';
 
-// Add the `toTitleCase` method to the String prototype
 if (typeof String.prototype.toTitleCase !== 'function') {
   String.prototype.toTitleCase = function(options) {
     const titleCaser = new TitleCaser(options);
@@ -9,10 +7,8 @@ if (typeof String.prototype.toTitleCase !== 'function') {
   };
 }
 
-// Export the `TitleCaser` class for other modules to use
 export { TitleCaser };
 
-// Add the `TitleCaser` class to the global object in the browser environment
 if (typeof window === 'object') {
   window.TitleCaser = TitleCaser;
 }
