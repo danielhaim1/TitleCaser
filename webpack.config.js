9a6fa7cd8c42f84d0283419626dcb36df3745767
Daniel Haim <2808046+danielhaim1@users.noreply.github.com>
2023-04-05 11:26:00 +0200

    demo + amd

--- webpack.config.js
+++ webpack.config.js
@@ -1,64 +1,66 @@
-const path = require('path');
-const isDevelopment = process.env.NODE_ENV === 'development';
-const nodeExternals = require('webpack-node-externals');
-const TerserPlugin = require('terser-webpack-plugin');
-
-module.exports = [
-  {
-    mode: 'production',
-    target: 'node',
-    entry: './src/index.js',
-    output: {
-      filename: 'titlecaser.js',
-      path: path.resolve(__dirname, 'dist'),
-    },
-    optimization: {
-      minimize: true,
-      minimizer: [new TerserPlugin()],
-    },
-    module: {
-      rules: [
-        {
-          test: /\.js$/,
-          exclude: /(node_modules|bower_components)/,
-          use: {
-            loader: 'babel-loader',
-            options: {
-              presets: [['@babel/preset-env']],
-            },
-          },
-        },
-      ],
-    },
-    externals: nodeExternals({
-      allowlist: ['@babel/runtime-corejs3'],
-    }),
+const path = require("path"),
+  isDevelopment = "development" === process.env.NODE_ENV,
+  nodeExternals = require("webpack-node-externals"),
+  TerserPlugin = require("terser-webpack-plugin");
+  
+module.exports = [{
+  mode: "production",
+  target: "web",
+  entry: "./src/index.js",
+  output: {
+    filename: "titlecaser.browser.js",
+    path: path.resolve(__dirname, "dist")
   },
-  {
-    mode: 'production',
-    target: 'web',
-    entry: './src/index.js',
-    output: {
-      filename: 'titlecaser.browser.js',
-      path: path.resolve(__dirname, 'dist'),
-    },
-    optimization: {
-      minimize: true,
-      minimizer: [new TerserPlugin()],
-    },
-    module: {
-      rules: [
-        {
-          test: /\.js$/,
-          exclude: /(node_modules|bower_components)/,
-          use: {
-            loader: 'babel-loader',
-            options: {
-              presets: [['@babel/preset-env', { modules: false }]],
-            },
-          },
-        },
-      ],
-    },
+  optimization: {
+    minimize: !0,
+    minimizer: [new TerserPlugin]
   },
-];
+  module: {
+    rules: [{
+      test: /\.js$/,
+      exclude: /(node_modules|bower_components)/,
+      use: {
+        loader: "babel-loader",
+        options: {
+          presets: [["@babel/preset-env", {
+            targets: {
+              browsers: ["last 2 versions", "safari >= 7"]
+            },
+            modules: "amd"
+          }, ], ]
+        }
+      }
+    }, ]
+  }
+}, {
+  mode: "production",
+  target: "node",
+  entry: "./src/index.js",
+  output: {
+    filename: "titlecaser.js",
+    path: path.resolve(__dirname, "dist"),
+    libraryTarget: "commonjs2"
+  },
+  optimization: {
+    minimize: !0,
+    minimizer: [new TerserPlugin]
+  },
+  module: {
+    rules: [{
+      test: /\.js$/,
+      exclude: /(node_modules|bower_components)/,
+      use: {
+        loader: "babel-loader",
+        options: {
+          presets: [["@babel/preset-env", {
+            targets: {
+              node: "10"
+            },
+            modules: "commonjs"
+          }, ], ]
+        }
+      }
+    }, ]
+  },
+  externals: [nodeExternals()]
+}, ];
